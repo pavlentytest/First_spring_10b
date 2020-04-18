865cbc1607848944f1c496c6233d28f21d047c37
Pavel <impetus@yandex.ru>
2020-04-18 11:07:45 +0300

    first commit

---
+++ src/main/java/ru/sch2/b/Controller.java
@@ -0,0 +1,30 @@
+package ru.sch2.b;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+
+public class Controller {
+
+    /*
+    GET  - получить http://127.0.0.1/?name=Ivan&email=test@yandex.ru
+    Корневой запрос: http://127.0.0.1/
+
+
+    POST  - отправить http://127.0.0.1/
+                     в теле запроса: name=Ivan&email=test@yandex.ru
+     */
+
+    @GetMapping(path="/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping(path="/about")
+    public String about() {
+        return "about";
+    }
+
+
+
+}
---
+++ src/test/java/ru/sch2/b/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sch2.b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
